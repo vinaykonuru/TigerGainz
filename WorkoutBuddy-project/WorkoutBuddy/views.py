a651eb92fdb3b84010cc9d6d2bbe8188d283fab7
Vinay Konuru <vinaykonuru@gmail.com>
2020-07-19 21:09:55 -0400

    got requests list into dataframe

--- WorkoutBuddy-project/WorkoutBuddy/views.py
+++ WorkoutBuddy-project/WorkoutBuddy/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from buddyrequest.models import BuddyRequest
 import pandas
 def home(request):
+    return render(request,'home.html')
+
+@login_required(login_url='/accounts/signup')
+def find(request):
+    return render(request,'find.html')
+
+@login_required(login_url='/accounts/signup')
+def waiting(request):
     if request.method=='POST':
         #get data about USER
         userdatadf=pandas.read_csv('WorkoutBuddy\studentdata.csv',index_col=('netID'))
@@ -18,25 +27,32 @@ def home(request):
         time_zone=request.POST['time_zone']
         group_size=request.POST['group_size']
 
-        #put into csv
-        f = open("user.csv", "w")
-        headers='Name,Day_Av,Duration,Type_Workout,Time_z,No_Ppl \n'
-        data=''+name+','+days+','+duration+','+workout_type+','+time_zone+','+group_size+'\n'
-        f.write(headers)
-        f.write(data)
-        #formula
-        #do something in the requests list
-        return redirect('waiting')
-    else:
-        return render(request,'home.html')
+        #list of data
+        user_data_list=[name,days,duration,workout_type,time_zone,group_size]
 
-@login_required(login_url='/accounts/signup')
-def find(request):
-    return render(request,'find.html')
+        user_data_list_df=pandas.DataFrame(user_data_list)
+        #list of requests in dataframe
+        requestsList=list(BuddyRequest.objects.all().values())
+        #dataframe of requests
+        requestsdf=pandas.DataFrame(requestsList)
+        print(requestsdf)
+        #COMPARISION BETWEEN USER DATA AND REQUESTS ENTERED HERE
 
-@login_required(login_url='/accounts/signup')
-def waiting(request):
-    return render(request,'waiting.html')
+
+        #IF NO MATCH IS FOUND, ADD NEW REQUEST
+        if match==False:
+            req=BuddyRequest(name=name,major=major,year=year,rescollege=rescollege,days=days,duration=duration,
+            workout_type=workout_type,time_zone=time_zone,group_size=group_size,user=request.user)
+            req.save()
+            return render(request,'waiting.html')
+
+        #IF MATCH IS FOUND, GET DATA,  REMOVE IT FROM REQUESTS AND PUT IT IN A GROUP
+        else:
+            matchedName=name
+            matchedRequest=BuddyRequest.objects.filter(name=matchedName)
+            return render(request,'waiting.html',{'matched_person_name':matchedName})
+
+
 
 @login_required(login_url='/accounts/signup')
 def about(request):
--- WorkoutBuddy-project/buddyrequest/models.py
+++ WorkoutBuddy-project/buddyrequest/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import json
 # Create your models here.
 class BuddyRequest(models.Model):
@@ -10,6 +11,7 @@ class BuddyRequest(models.Model):
     duration=models.CharField(max_length=50)
     workout_type=models.CharField(max_length=50)
     time_zone=models.CharField(max_length=50)
+    group_size=models.IntegerField(default=2)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
 
     def set_days(self,x):
