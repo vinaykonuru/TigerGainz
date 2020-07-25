029a5e288c02e639b83332111bcc37cceffcc997
Eugeneliu86 <67016870+Eugeneliu86@users.noreply.github.com>
2020-07-24 23:01:35 -0400

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,41 +1,38 @@
-import smtplib, ssl
+import smtplib
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def mail (name, netID, user=False):
 
+def mail(name, netID, user=False):
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Your New Workout Buddy"
 
     sent_email = netID + "@princeton.edu"
-
     tiger_gainz_pass = "WorkoutTinder123"
     tiger_gainz_email = "no.reply.tigergainz@gmail.com"
 
- 
-    if user==False:
+    if user == False:
 
         text = f'''\
         Hi!
 
         We are so excited to present you with your new workout partner, {name}! You can contact them via email: {sent_email}
 
-
         Happy exercising,
-
         TigerGainz'''
-        
-        
-     else:
-        
+
+
+    else:
+
         text = f'''\
         Hi!
 
         We are sending a confirmation email regarding your new workout partner, {name}! You can contact them via email: {sent_email}
 
-
         Happy exercising,
-
         TigerGainz'''
-        
-        
+
     message.attach(MIMEText(text, "plain"))
 
     with smtplib.SMTP("smtp.gmail.com", 587) as s:
@@ -43,9 +40,3 @@ def mail (name, netID, user=False):
         s.login(tiger_gainz_email, tiger_gainz_pass)
         s.sendmail(tiger_gainz_email, sent_email, message.as_string())
         s.quit()
-
-#import data
-
-    #import names of both people that got matched
-    
-
