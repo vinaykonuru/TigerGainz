dde02a0a3e891fe875c6a9373b725737d2a6a09a
Vinay Konuru <vinaykonuru@gmail.com>
2020-10-09 19:59:38 -0400

    updated settings and wsgi for heroku

--- WorkoutBuddy-project/WorkoutBuddy/settings.py
+++ WorkoutBuddy-project/WorkoutBuddy/settings.py
@@ -131,6 +131,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 try:
     from .local_settings import *
 except ImportError:
