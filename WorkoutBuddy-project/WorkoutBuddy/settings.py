26d74987f847864d412f098195e2e913a5370449
Vinay Konuru <vinaykonuru@gmail.com>
2020-10-09 21:20:31 -0400

    added whitenoise dependency

--- WorkoutBuddy-project/WorkoutBuddy/settings.py
+++ WorkoutBuddy-project/WorkoutBuddy/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django-heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -132,6 +132,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
 
 try:
     from .local_settings import *
