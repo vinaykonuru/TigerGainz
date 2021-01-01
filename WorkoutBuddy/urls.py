818d9515c914dbed0d36ef196f8caf02e7755da7
Vinay Konuru <vkonuru@princeton.edu>
2021-01-01 08:46:34 -0500

    removed invalid import

--- WorkoutBuddy/urls.py
+++ WorkoutBuddy/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from uniauth.urls import cas-only
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
