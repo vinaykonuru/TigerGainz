ce2731994f52f5a18c4d1a224767debdfc04938f
Vinay Konuru <vkonuru@princeton.edu>
2021-01-05 15:04:12 -0500

    made secret key environment variable in Heroku

--- WorkoutBuddy/settings.py
+++ WorkoutBuddy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l#lfb-a-+=&_j#dh&v_&du6m(_ct)r1h0=(9$12e#suu7k#_s1'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
