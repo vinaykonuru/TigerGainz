5909175edec5324ab077035e8a274fa949adb17b
Vinay Konuru <vkonuru@princeton.edu>
2021-01-07 20:32:21 -0500

    test

--- buddyrequest/views.py
+++ buddyrequest/views.py
@@ -104,6 +104,7 @@ def matches(request):
             return render(request,'buddyrequest/matches.html')
         else:
             matched_people=get_matches(user_data_list, requestsList)
+            print(matched_people)
             if len(matched_people) > 3: # if more than 3 matches, give best 3
                 return render(request,'buddyrequest/matches.html', {'matched_people':matched_people[0:2]})
 
