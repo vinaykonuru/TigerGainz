27a0952fe872fd9935ac266dd5de08ce7404c122
Vinay Konuru <vkonuru@princeton.edu>
2021-01-10 16:14:38 -0500

    test

--- buddyrequest/views.py
+++ buddyrequest/views.py
@@ -19,8 +19,9 @@ def database(request):
     profiles = []
     for entry in buddyrequests:
         if(entry.partner == None and entry.user != request.user):
-            print("Workout Type: " + str(entry.workout_type))
             workout_type = set(entry.workout_type)
+            print("Workout Type: " + str(workout_type))
+            print(workout_type_filter.issubset(workout_type))
             if(workout_type_filter.issubset(workout_type)):
                 profiles.append(entry)
 
