6a9712671e0300749f02d0013f1dc54fb56485bd
Vinay Konuru <vkonuru@princeton.edu>
2021-01-13 13:36:28 -0500

    timezone filter

--- buddyrequest/views.py
+++ buddyrequest/views.py
@@ -15,15 +15,15 @@ def database(request):
     buddyrequests=BuddyRequest.objects.all()
     workout_type_filter = set(request.POST.getlist('workout_type_filter'))
     duration_filter = set(request.POST.getlist('duration_filter'))
-
+    time_zone_filter = set(request.POST.getlist('time_zone_filter'))
     profiles = []
     for entry in buddyrequests:
         if(entry.partner == None and entry.user != request.user):
             workout_type = set(entry.workout_type.strip('][\'').split(','))
             duration = set(entry.duration.strip('][\'').split(','))
-            print(duration)
-            print(duration_filter)
-            if(workout_type_filter.issubset(workout_type) & duration_filter.issubset(duration)):
+            timezone = set(entry.timezone.strip('][\'').split(','))
+            if(workout_type_filter.issubset(workout_type) & duration_filter.issubset(duration)\
+            & time_zone_filter.issubset(timezone)):
                 profiles.append(entry)
 
     return render(request,'buddyrequest/database.html',{'profiles':profiles})
