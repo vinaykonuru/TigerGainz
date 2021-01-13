8d1dedc02cd5f24b5aceaa3ec6248244b4b92cf7
Vinay Konuru <vkonuru@princeton.edu>
2021-01-13 16:39:15 -0500

    test

--- buddyrequest/views.py
+++ buddyrequest/views.py
@@ -28,7 +28,9 @@ def database(request):
             if(workout_type_filter_set.issubset(workout_type) & duration_filter_set.issubset(duration)\
             & time_zone_filter_set.issubset(timezone)):
                 profiles.append(entry)
-
+    str(workout_type_filter).strip('][')
+    str(duration_filter).strip('][')
+    str(time_zone_filter).strip('][')
     return render(request,'buddyrequest/database.html',{'profiles':profiles,'time_zone_filter':\
     time_zone_filter,'workout_type_filter':workout_type_filter,'duration_filter':duration_filter})
 def remove_request(request):
