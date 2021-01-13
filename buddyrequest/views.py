637cb6e9f8804c8514dd740f06e4b7ff9df622e5
Vinay Konuru <vkonuru@princeton.edu>
2021-01-13 13:23:52 -0500

    test

--- buddyrequest/views.py
+++ buddyrequest/views.py
@@ -14,17 +14,16 @@ from .tigerhub_access import getStudentInfo
 def database(request):
     buddyrequests=BuddyRequest.objects.all()
     workout_type_filter = set(request.POST.getlist('workout_type_filter'))
-    duration_filter = set(request.POST.getlist('duration_filter'))
+    duration_filter = request.POST['duration_filter']
 
     profiles = []
     for entry in buddyrequests:
         if(entry.partner == None and entry.user != request.user):
             workout_type = set(entry.workout_type.strip('][\'').split(','))
-            duration = set(entry.duration)
+            duration = entry.duration
             print(duration)
             print(duration_filter)
-            if(workout_type_filter.issubset(workout_type) &\
-             duration_filter.issubset(duration)):
+            if(workout_type_filter.issubset(workout_type) & duration == duration_filter):
                 profiles.append(entry)
 
     return render(request,'buddyrequest/database.html',{'profiles':profiles})
