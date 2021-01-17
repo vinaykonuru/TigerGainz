f11d4aaf6f08bb6e950208deb81daf509f58078e
Vinay Konuru <vkonuru@princeton.edu>
2021-01-17 17:52:30 -0500

    change static root to media root

--- WorkoutBuddy/urls.py
+++ WorkoutBuddy/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('database/',include('buddyrequest.urls')),
     path('partners/',include('buddyrequest.urls')),
     path('contacts/',views.contacts,name='contacts')
-] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
