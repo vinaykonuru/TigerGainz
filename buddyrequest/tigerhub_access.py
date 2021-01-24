5d9a5cd5c1672858928d9fb2cd9dbcce96196e92
Vinay Konuru <vkonuru@princeton.edu>
2021-01-24 13:26:50 -0500

    added config variable for tigerbook key

--- buddyrequest/tigerhub_access.py
+++ buddyrequest/tigerhub_access.py
@@ -3,13 +3,14 @@ import random
 from base64 import b64encode
 import datetime
 import requests
+import os
 
 def getStudentInfo(id):
     url = 'https://tigerbook.herokuapp.com/api/v1/undergraduates/' + id
     created = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
     nonce = ''.join([random.choice('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/=') for i in range(32)])
     username = 'vkonuru'
-    password = '654da355c8637554723841049f02e537'
+    password = os.environ.get('TIGERBOOK_KEY')
 
     generated_digest = b64encode(hashlib.sha256((nonce + created + password).encode('utf-8')).digest()).decode('utf-8')
 
