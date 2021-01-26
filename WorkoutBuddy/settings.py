8bcd8573bc86a21b2d108bbc91d5d16efafd4f91
Vinay Konuru <vkonuru@princeton.edu>
2021-01-26 13:19:09 -0500

    production settings

--- WorkoutBuddy/settings.py
+++ WorkoutBuddy/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','tigergainz.herokuapp.com']
 
 
 # Application definition
