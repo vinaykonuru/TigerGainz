be4fffdac11ff077054f13750cedb08ef9e6ee23
Vinay Konuru <vkonuru@princeton.edu>
2021-01-27 09:29:43 -0500

    fix logic

--- buddyrequest/matching_algorithm.py
+++ buddyrequest/matching_algorithm.py
@@ -225,26 +225,25 @@ def get_matches(user_data_list, requests_list):
         if(workout_percentage != 0 and len(list_best_match_vals) != 0):
             average = (mean(list_best_match_vals) + workout_percentage)/2
             list_best_match_vals.append(average)
-            
+            list_best_match_vals.append(Dfrq_index)
             print('LIST BEST MATCH VALUES')
             print(list_best_match_vals)
             ListOfMatches.append(list_best_match_vals) #last element of each sublist is the index of that row in the database
-            ListOfMatches.append(Dfrq_index)
+
         #ListOfMatches is a nested list containing
     print(ListOfMatches)
     n = 0
     while n < len(ListOfMatches): #will stop the loop when we have looped through n-1 times
         n += 1 #counter that ensures we are below n
         for i in list(range(len(ListOfMatches) - 1)): #for every index value in list of index values
-          if ListOfMatches[i][-1] < ListOfMatches[i+1][-1]: #Conditional statement that compares if i and its adjacent value
+          if ListOfMatches[i][-2] < ListOfMatches[i+1][-2]: #Conditional statement that compares if i and its adjacent value
                                                             #-2 ensures that the sorting is based on the final weighted average
                                                             #for each row
             ListOfMatches[i], ListOfMatches[i+1] = ListOfMatches[i+1], ListOfMatches[i] #swaps if adjacent value is smaller
 
     row_index = []
     for entry in ListOfMatches:
-        val = entry[-1]
-        row_index.append(val)
+        row_index.append(entry[-1])
 
     presentation_list = []
     print(row_index)
