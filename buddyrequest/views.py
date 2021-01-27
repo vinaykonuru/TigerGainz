1f3ed119ce8ba5c655e74708f8afb52ef127547d
Vinay Konuru <vkonuru@princeton.edu>
2021-01-27 10:51:28 -0500

    entry is dict

--- buddyrequest/views.py
+++ buddyrequest/views.py
@@ -108,7 +108,7 @@ def matches(request):
         #if the user already has a request or partner in the database, go back to home page
         requestsList=list(BuddyRequest.objects.all().values())
         for entry in requestsList:
-            if entry.partner != None:
+            if entry['partner'] != None:
                 requestsList.remove(entry)
         netID = request.user.uniauth_profile.get_display_id()
         for entry in requestsList:
